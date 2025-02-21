fe32e2c8bfb63ec3e961ea52ae2fa15a2a8a90ca
Bhargav Chataut <bhargavchataut101@gmail.com>
2025-02-21 15:11:20 -0600

    Implemented bubble sort in go

--- bubble_sort/go/main.go
+++ bubble_sort/go/main.go
@@ -5,5 +5,25 @@ import (
 )
 
 func main() {
-	fmt.Println("This is a Bubble Sort algorithm")
+	fmt.Println("This is a Bubble Sort")
+	bubble_sort()
+}
+
+func bubble_sort() {
+	var numbers []uint8 = []uint8{5, 4, 3, 1, 8, 6, 9, 7, 2}
+	fmt.Printf("Before sorting: %v\n", numbers)
+
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 8-i; j++ {
+			if numbers[j] > numbers[j+1] {
+				var temp uint8 = numbers[j+1]
+				numbers[j+1] = numbers[j]
+				numbers[j] = temp
+			}
+		}
+
+	}
+
+	fmt.Printf("After sorting: %v", numbers)
+
 }

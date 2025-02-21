21d82adeb8f33ff4d6d3dd739ef48d6ae161ac27
Bhargav Chataut <bhargavchataut101@gmail.com>
2025-02-21 16:01:37 -0600

    Implemented a random number's list generator

--- bubble_sort/go/main.go
+++ bubble_sort/go/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func main() {
@@ -10,13 +11,14 @@ func main() {
 }
 
 func bubble_sort() {
-	var numbers []uint8 = []uint8{5, 4, 3, 1, 8, 6, 9, 7, 2}
+
+	var numbers []int = random_arr(10)
 	fmt.Printf("Before sorting: %v\n", numbers)
 
 	for i := 0; i < 8; i++ {
 		for j := 0; j < 8-i; j++ {
 			if numbers[j] > numbers[j+1] {
-				var temp uint8 = numbers[j+1]
+				var temp int = numbers[j+1]
 				numbers[j+1] = numbers[j]
 				numbers[j] = temp
 			}
@@ -26,4 +28,12 @@ func bubble_sort() {
 
 	fmt.Printf("After sorting: %v", numbers)
 
+}
+
+func random_arr(size int) []int {
+	random_list := make([]int, size)
+	for i := 0; i < size; i++ {
+		random_list[i] = rand.Intn(10)
+	}
+	return random_list
 }

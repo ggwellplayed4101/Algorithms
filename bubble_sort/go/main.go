c5cf6894135b08dce0ae4f50651da6b30145a475
Bhargav Chataut <bhargavchataut101@gmail.com>
2025-02-22 16:15:04 -0600

    Seperated generation of list in one function

--- bubble_sort/go/main.go
+++ bubble_sort/go/main.go
@@ -1,3 +1,5 @@
+// Bubble Sort
+
 package main
 
 import (
@@ -7,33 +9,36 @@ import (
 
 func main() {
 	fmt.Println("This is a Bubble Sort")
-	bubble_sort()
+
+	const size int = 1000
+	var unsorted_list []int = random_arr(size)
+
+	fmt.Println("The size of the list is", size)
+	fmt.Println("Before Sorting:", unsorted_list[0:10], "...", unsorted_list[size-10:])
+	var sorted_list []int = bubble_sort(unsorted_list, size)
+	fmt.Println("After Sorting:", sorted_list[0:10], "...", sorted_list[size-10:])
+
 }
 
-func bubble_sort() {
+func bubble_sort(numbers []int, length int) []int {
 
-	var numbers []int = random_arr(10)
-	fmt.Printf("Before sorting: %v\n", numbers)
-
-	for i := 0; i < 8; i++ {
-		for j := 0; j < 8-i; j++ {
+	for i := 0; i < length-1; i++ {
+		for j := 0; j < length-1-i; j++ {
 			if numbers[j] > numbers[j+1] {
 				var temp int = numbers[j+1]
 				numbers[j+1] = numbers[j]
 				numbers[j] = temp
 			}
 		}
-
 	}
-
-	fmt.Printf("After sorting: %v", numbers)
+	return numbers
 
 }
 
 func random_arr(size int) []int {
 	random_list := make([]int, size)
 	for i := 0; i < size; i++ {
-		random_list[i] = rand.Intn(10)
+		random_list[i] = rand.Intn(10001)
 	}
 	return random_list
 }

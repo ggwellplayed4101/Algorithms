0925d354ed0137cff36001a4543e1aa7ed903e1d
Bhargav Chataut <bhargavchataut101@gmail.com>
2025-02-22 17:00:17 -0600

    Benchmarked using time package

--- bubble_sort/go/main.go
+++ bubble_sort/go/main.go
@@ -5,23 +5,30 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
+
+const size int = 1000
+
+var unsorted_list []int = random_arr(size)
 
 func main() {
 	fmt.Println("This is a Bubble Sort")
 
-	const size int = 1000
-	var unsorted_list []int = random_arr(size)
-
 	fmt.Println("The size of the list is", size)
 	fmt.Println("Before Sorting:", unsorted_list[0:10], "...", unsorted_list[size-10:])
-	var sorted_list []int = bubble_sort(unsorted_list, size)
+
+	start := time.Now()
+	var sorted_list []int = bubble_sort(unsorted_list)
+	elapsed := time.Since(start).Seconds()
+
 	fmt.Println("After Sorting:", sorted_list[0:10], "...", sorted_list[size-10:])
+	fmt.Println("Execution time:", elapsed, "seconds")
 
 }
 
-func bubble_sort(numbers []int, length int) []int {
-
+func bubble_sort(numbers []int) []int {
+	length := len(numbers)
 	for i := 0; i < length-1; i++ {
 		for j := 0; j < length-1-i; j++ {
 			if numbers[j] > numbers[j+1] {

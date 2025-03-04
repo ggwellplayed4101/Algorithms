8223544bc31d15e22e85bd654f6fbfc956632890
Bhargav Chataut <bhargavchataut101@gmail.com>
2025-03-04 00:08:43 -0600

    added function to show info about a given list

--- list_generator.py
+++ list_generator.py
@@ -2,3 +2,8 @@ import random
 
 def list_generator (size=1000, min=0, max=10000):
     return [random.randint(min, max) for _ in range(size)]
+
+def print_list_details(lst):
+    print(f"List length: {len(lst)}")
+    print(f"First 10 numbers: {lst[:10]}")
+    print(f"Last 10 numbers: {lst[-10:]}")

76a91548f66764b841f0e80529f0a29737e78ffd
Bhargav Chataut <bhargavchataut101@gmail.com>
2025-03-03 23:14:21 -0600

    Implemented time taken

--- shell_sort/python/shell_sort.py
+++ shell_sort/python/shell_sort.py
@@ -1,3 +1,17 @@
+import time
+from list_generator import list_generator
+
+def main():
+    unsorted_list = list_generator()
+
+    start = time.time()
+    sorted_list = shell_sort(unsorted_list)
+    end = time.time()
+
+    elapsed_time = end - start
+
+    print(f"Time taken by shell_sort: {elapsed_time:.7f} seconds")
+
 def shell_sort(list):
     gap = len(list) // 2
 
@@ -12,6 +26,7 @@ def shell_sort(list):
             
             list[j + gap] = temp
         gap //= 2
-        return list
+    return list
 
-        
+if __name__ == "__main__":
+    main()      
